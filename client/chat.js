5646aa9d30c355986db804128399471d034fcb73
Samuel Chan <83254763+ChanSamuel@users.noreply.github.com>
2022-02-17 17:13:38 +1300

    Setup the sockets on client and server ends so that the user can now submit messages to the server which prints them to the server-side console.

--- client/chat.js
+++ client/chat.js
@@ -1,2 +1,19 @@
-document.getElementById('test-id').innerHTML = 'Replaced text';
-console.log('did something');
+// Open a socket to the host server.
+const socket = io('ws://localhost:3000');
+
+
+var form = document.getElementById('chatbox-form');
+var textbox = document.getElementById('text-box');
+
+form.addEventListener('submit', (e) => {
+
+    // Cancel the form submission operation so that the page doesn't reload upon submitting the message.
+    e.preventDefault();
+
+    // If the text box is not empty, send the message to the server via the socket.
+    if (textbox.value) {
+        socket.emit('message', textbox.value);
+        textbox.value = '';
+    }
+
+});
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const SERVER_PORT = process.env.PORT || 3000; // Use the configured port if it exists, otherwise use port 3000.
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+// Configure the home route to the home.html page.
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/home.html');
 })
 
+// Need this otherwise filepaths on the client-side will not work as intended.
 app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {console.log('Listening on port ' + PORT);});
+// Begin listening for socket connections.
+io.on('connection', socket => {
+    console.log('A user has connected.');
+
+    // Acknowledge connection success by sending a 'connected' event via this socket connection.
+    socket.send('connected');
+
+    // Configure a listener to this socket which listens for chat messages. 
+    socket.on('message', (msg) => {
+        console.log(msg);
+    });
+
+    // Configure a listener to this socket which listens for client disconnect.
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected');
+    });
+});
+
+// Set the HTTP server to begin listening for requests.
+server.listen(SERVER_PORT);
+console.log('Listening on port ' + SERVER_PORT);
